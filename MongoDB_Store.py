39ce524c5d88edc1b3e2418b0a5523811d77e8f2
guitarliu <42517120+guitarliu@users.noreply.github.com>
2022-01-11 18:48:46 +0800

    数据库数据批量写入脚本

---
+++ MongoDB_Store.py
@@ -0,0 +1,50 @@
+# coding: utf-8
+
+from pymongo import MongoClient
+from openpyxl import *
+import os
+
+
+client = MongoClient()
+db = client.standardDatabase
+
+for root, dir, files in os.walk("C://Users//xiaotaiye//Desktop//02S403"):
+    for filename in files:
+        if ".txt" in filename:
+            with open(root + "/" + filename, "r") as f:
+                list_page_elements = []
+                page_elements = [e.strip("\n") for e in f.readline().split(",")]
+                for i in f.readlines():
+                    list_page_elements.append([j.strip("\n") for j in i.split(",")])
+            print(filename, page_elements,list_page_elements)
+            for row in list_page_elements:
+                dict_page_elements = {}
+                for k,v in zip(page_elements,row):
+                    dict_page_elements[k]=float(v)
+                print(dict_page_elements)
+                post = {"standard_name":filename.split(",")[0],
+                    "pages":filename.split(",")[1],
+                    "page_title": filename.split(",")[2].rstrip(".txt"),
+                    "standard_data":dict_page_elements
+                }
+                db.standards.insert_one(post)
+        elif ".xlsx" in filename:
+            wb = load_workbook(root + "/" + filename)
+            ws = wb.active
+            list_page_elements = []
+            for row in ws.iter_rows(min_row=1,max_row=1,values_only=True):
+                page_elements = row
+            for else_row in ws.iter_rows(min_row=2,values_only=True):
+                list_page_elements.append(else_row)
+            print(filename, page_elements,list_page_elements)
+            for row in list_page_elements:
+                dict_page_elements = {}
+                for k,v in zip(page_elements,row):
+                    dict_page_elements[k]=float(v)
+                print(dict_page_elements)
+                post = {"standard_name":filename.split(",")[0],
+                    "pages":filename.split(",")[1],
+                    "page_title": filename.split(",")[2].rstrip(".xlsx"),
+                    "standard_data":dict_page_elements
+                }
+                db.standards.insert_one(post)
